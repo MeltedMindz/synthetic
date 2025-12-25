2b66425856fc03a32ad9e1da499b28dea1446f7d
MeltedMindz <95740781+MeltedMindz@users.noreply.github.com>
2025-12-25 10:39:58 -0800

    Fix compilation errors: add missing using directives and assembly references
    - Add SyntheticLife.Phi.Config using to Genome.cs and GrowthSystem.cs
    - Create assembly definition file with Physics module reference
    - Fixes CS1069 errors for Collider/Rigidbody types
    - Fixes CS0246 errors for CreatureDefaults type

--- Assets/Scripts/Core/Genome.cs
+++ Assets/Scripts/Core/Genome.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using SyntheticLife.Phi.Config;
 
 namespace SyntheticLife.Phi.Core
 {
--- Assets/Scripts/Core/GrowthSystem.cs
+++ Assets/Scripts/Core/GrowthSystem.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using SyntheticLife.Phi.Config;
 
 namespace SyntheticLife.Phi.Core
 {
